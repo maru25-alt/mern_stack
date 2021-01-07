d2f1683b653fb2b417750979d46209198f04c620
Noel <chaoticforbidden@gmail.com>
2021-01-07 16:40:21 -0600

    Setting up sign up and login page for clinics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
 import SignUpForm from "./components/singup/Signup";
 import Ads from "./components/ads/Ads";
-=======
->>>>>>> f5324eb... set commit for sign up
 import useAuth from "./hooks/useAuth";
 
 function App() {
