2d215efbbc6d14626bfd4cf2df5f8d3a4b8a3789
Christopher Davis <cdav49719@gmail.com>
2021-01-09 22:18:27 -0500

    one cluster for signup endpoints filter by type user/clinic

--- api/routes/Clinic.js
+++ api/routes/Clinic.js
@@ -4,17 +4,24 @@ const express = require("express");
 const route = express.Router();
 
 route.post("/", async (req, res) => {
-  const { name, password, address, url } = req.body;
+  const { name, password, email } = req.body;
   let clinic = {};
 
   clinic.name = name;
+  clinic.email = email;
+
   clinic.password = password;
-  clinic.address = address;
-  clinic.url = url;
+  clinic.account = "clinic";
 
   let clinicModel = new ClinicModel(clinic);
   await clinicModel.save();
   res.json(clinic);
+});
+
+route.get("/", async (req, res) => {
+  const accounts = await ClinicModel.find();
+  const clinics = accounts.filter((u) => u.account === "clinic");
+  res.json(clinics);
 });
 
 module.exports = route;
--- api/routes/User.js
+++ api/routes/User.js
@@ -3,17 +3,27 @@ import UsersModel from "../../db/models/UsersModel";
 const express = require("express");
 const route = express.Router();
 
+const users = [];
+
 route.post("/", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+  const { name, email, password } = req.body;
   let user = {};
 
-  user.firstName = firstName;
-  user.lastName = lastName;
+  user.name = name;
   user.email = email;
   user.password = password;
+  user.account = "user";
+
   let userModel = new UsersModel(user);
   await userModel.save();
   res.json(userModel);
+});
+
+route.get("/", async (req, res) => {
+  const accounts = await UsersModel.find();
+  const user = accounts.filter((u) => u.account === "user");
+
+  res.json(user);
 });
 
 module.exports = route;
